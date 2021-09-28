fb68e21f3cc7d4f866c80215dfc0ddf55f3f59b1
akash <mr.akash857@gmail.com>
2021-09-28 10:44:01 +0530

    second commit

--- src/main/java/com/example/demo/entity/Specialization.java
+++ src/main/java/com/example/demo/entity/Specialization.java
@@ -1,6 +1,10 @@
 package com.example.demo.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -9,5 +13,19 @@ import lombok.Data;
 @Data
 @Table(name="specialization_tab")
 public class Specialization {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="spec_id_col")
+	private Long id;
+	
+	@Column(name="spec_name_col")
+	private String specName;
+	
+	@Column(name="spec_code_col")
+	private String specCode;
+	
+	@Column(name="spec_note_col")
+	private String specNote;
 
 }
