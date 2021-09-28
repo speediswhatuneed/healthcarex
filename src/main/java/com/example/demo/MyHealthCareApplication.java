1341ea6e4155eca19c2023af9e82048aff4f4291
akash <mr.akash857@gmail.com>
2021-09-28 10:29:55 +0530

    First commit

---
+++ src/main/java/com/example/demo/MyHealthCareApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHealthCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHealthCareApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/entity/Specialization.java
@@ -0,0 +1,13 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="specialization_tab")
+public class Specialization {
+
+}
